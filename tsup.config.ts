b05d38f3662980a909867d3f2513800e14334a03
Salem <spencernavarro2004@gmail.com>
2024-07-30 23:04:30 -0700

    feat(dependencies): Updated dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-    format: ["cjs", "esm"],
-    entry: ["./src/index.ts"],
-    dts: true,
-    minify: true,
-    target: "esnext",
-    bundle: true,
-    sourcemap: true,
-    keepNames: true,
-    shims: true,
-    skipNodeModulesBundle: true,
-    clean: true,
-    ignoreWatch: ["**/node_modules/**"]
+	format: ["cjs", "esm"],
+	entry: ["./src/index.ts"],
+	dts: true,
+	minify: true,
+	target: "esnext",
+	bundle: true,
+	sourcemap: true,
+	keepNames: true,
+	shims: true,
+	skipNodeModulesBundle: true,
+	clean: true,
+	ignoreWatch: ["**/node_modules/**"]
 });
