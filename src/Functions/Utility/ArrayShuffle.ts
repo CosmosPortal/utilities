201d868829951561547ac6725d29b46962abe28c
Salem <spencernavarro2004@gmail.com>
2024-08-08 16:45:19 -0700

    chore(prettier): Ran prettier check

--- src/Functions/Utility/ArrayShuffle.ts
+++ src/Functions/Utility/ArrayShuffle.ts
@@ -4,12 +4,12 @@
  * @returns {T[]} The shuffled array
  */
 export function ArrayShuffle<T>(array: T[]): T[] {
-    const data = [...array]
+	const data = [...array];
 
-    for (let index = array.length - 1; index > 0; index--) {
-        const i = Math.floor(Math.random() * (index + 1));
-        [data[index], data[i]] = [data[i], data[index]];
-    }
+	for (let index = array.length - 1; index > 0; index--) {
+		const i = Math.floor(Math.random() * (index + 1));
+		[data[index], data[i]] = [data[i], data[index]];
+	}
 
-    return data;
+	return data;
 }
